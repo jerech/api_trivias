c5457b7a88679b3cbfc90b731b0849b04b5361e0
jerech <jeremiaschaparro@gmail.com>
2016-11-26 23:46:12 -0500

    Se agrega la verificacion de respuesta

--- GCMPushMessage.php
+++ GCMPushMessage.php
@@ -69,7 +69,7 @@ class GCMPushMessage {
 	    );
 
 	    $ch = curl_init ();
-	    curl_setopt($ch, CURLOPT_URL, $url);
+	    curl_setopt($ch, CURLOPT_URL, $this->url);
         curl_setopt($ch, CURLOPT_POST, true);
         curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
